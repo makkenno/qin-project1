56dfac3ecc89334e7f94197189ae856690633d6a
makkenno <kota.m.2774@gmail.com>
2022-08-22 21:26:32 +0900

    :sparkles: スマホ版contactページの実装

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,31 @@
 import type { NextPage } from "next";
-import { Layout, SectionTitle } from "lib/component";
-import { Space } from "@mantine/core";
+import { Space, Container, Stack, TextInput, Textarea } from "@mantine/core";
+import { Layout, SectionTitle, Button } from "lib/component";
+import { headerHeight, footerHeight } from "lib/const";
 
 const Contact: NextPage = () => {
   return (
     <Layout>
-      <Space h={40} />
-      <SectionTitle title="Contact" />
+      <Space h="xl" />
+      <Container
+        px="sm"
+        sx={{
+          height: `calc(100vh - ${headerHeight}px - ${footerHeight}px - 40px)`,
+        }}
+        className="w-full"
+      >
+        <SectionTitle title="Contact" />
+        <Space h="md" />
+        <Stack spacing="md">
+          <TextInput placeholder="Your@email.com" label="Email" />
+          <TextInput placeholder="Taro Yamada" label="Name" />
+          <Textarea
+            placeholder="I want to order your goods"
+            label="Your message"
+          />
+          <Button>Send Message</Button>
+        </Stack>
+      </Container>
     </Layout>
   );
 };
