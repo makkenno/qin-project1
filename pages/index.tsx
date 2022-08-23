9374e2af57acb3a35cac2c971d9822258ec13535
makkenno <kota.m.2774@gmail.com>
2022-08-23 20:22:25 +0900

    :lipstick: ヒーローレスポンシブ対応

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { Box, Title, Text, Space, Group, Stack } from "@mantine/core";
+import { Box, Title, Text, Space, Group, Stack, Center } from "@mantine/core";
 import { FaTwitter, FaFacebook, FaRss } from "react-icons/fa";
 import {
   Layout,
@@ -9,31 +9,45 @@ import {
   TwitterSection,
   Container,
 } from "lib/component";
+import { useMediaQuery } from "@mantine/hooks";
 
 const Home: NextPage = () => {
+  const matches = useMediaQuery("(max-width: 768px)");
   return (
     <Layout>
       <Box sx={(theme) => ({ background: theme.colors.pink[6], height: 248 })}>
         <Container>
-          <Space h={54} />
-          <Title order={2} className="text-white">
-            Makino Portfolio
-          </Title>
-          <Text
-            size={16}
-            mt={4}
-            weight={700}
-            color="white"
-            sx={{ fontFamily: "YuGothic" }}
-          >
-            まきののポートフォリオのためのページです
-          </Text>
-          <Space h={44} />
-          <Group spacing="sm">
-            <FaTwitter size={18} fill="white" />
-            <FaFacebook size={18} fill="white" />
-            <FaRss size={18} fill="white" />
-          </Group>
+          <Box className="flex items-center w-full" sx={{ height: 248 }}>
+            <Stack
+              spacing={matches ? 44 : 0}
+              className="w-full"
+              sx={{
+                flexDirection: matches ? "column" : "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box>
+                <Title order={2} className="text-white">
+                  Makino Portfolio
+                </Title>
+                <Text
+                  size={16}
+                  mt={4}
+                  weight={700}
+                  color="white"
+                  sx={{ fontFamily: "YuGothic" }}
+                >
+                  まきののポートフォリオのためのページです
+                </Text>
+              </Box>
+
+              <Group spacing="sm">
+                <FaTwitter size={18} fill="white" />
+                <FaFacebook size={18} fill="white" />
+                <FaRss size={18} fill="white" />
+              </Group>
+            </Stack>
+          </Box>
         </Container>
       </Box>
       <Space h="xl" />
