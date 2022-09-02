f918530aae9efa579a930d8af6d5ea30f0aee76c
makkenno <kota.m.2774@gmail.com>
2022-09-02 16:16:40 +0900

    🚑 API_KEYを変更し、envで管理

--- lib/client.ts
+++ lib/client.ts
@@ -3,5 +3,5 @@ import { createClient } from "microcms-js-sdk"; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "makino-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: "cd94bac5e8294297916a87c2351949aa4446",
+  apiKey: process.env.API_KEY,
 });
